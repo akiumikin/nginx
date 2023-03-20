27d62e29638301245243cf787d1fd1976d7f631a
akiumikin <akiumikin@gmail.com>
2023-03-20 15:05:49 +0000

    certの更新用のscriptを追加

---
+++ shell/certbot_update.sh
@@ -0,0 +1,3 @@
+certbot renew
+cp /etc/letsencrypt/live/server.akiumi.net/fullchain.pem ../certs/server.akiumi.net.crt
+cp /etc/letsencrypt/live/server.akiumi.net/privkey.pem ../certs/server.akiumi.net.key
